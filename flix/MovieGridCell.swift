d5727bd3d2a5c72a3ce89f77fd19f39be3e355cd
Veeresh Rajendran <veereshrajendran@gmail.com>
2020-10-07 20:41:55 -0400

    Added tab and view of superhero movies. Also added details of movie after clicking on one.

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Veeresh Rajendran on 10/7/20.
+//  Copyright © 2020 veereshrajendran@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
